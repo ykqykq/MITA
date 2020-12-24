c4e05a532e5c462e5dc1e36c072778c0cfaac962
ykqykq <915314967@qq.com>
2020-12-24 10:08:24 +0800

    Add files via upload

---
+++ CollisionMonitoring.java
@@ -0,0 +1,11 @@
+package utils;
+
+public class CollisionMonitoring {
+
+	static int offloadsize=7800;//卸载大小单位KB1200
+	static double revolution=18570;//需要的CPU转的次数，单位兆圈2857
+	static int receivesize=15;//返回的包大小
+	//static int prority=10;
+	static double ke=0.2;//能量占比
+	static double kt=0.8;//时间占比
+}
